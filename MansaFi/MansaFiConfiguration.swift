cb6b4cf20855a564e6a5416cb6728c3334df74dd
Mike <michael.westbrooks@redroostertec.com>
2017-12-17 09:52:52 -0500

    Initial Commit

---
+++ MansaFi/MansaFiConfiguration.swift
@@ -0,0 +1,19 @@
+//
+//  ConfigurationClass.swift
+//  MansaFi
+//
+//  Created by Michael Westbrooks II on 12/16/17.
+//  Copyright © 2017 RedRooster Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MansaFiConfiguration {
+    struct Plaid {
+        public let ClientName = "MansaFi"
+        public let SelectAccount = true
+        public let ClientKey = "5a35e4434e95b849199948fb"
+        public let PublicKey = "6f7ef85e09526cfd8cd90b3fe1d4cc"
+        public let SecretKey = "43a5ae0fecd28cb4fdaaf1274aa04d"
+    }
+}
---
+++ MansaFi/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  MansaFi
+//
+//  Created by Michael Westbrooks II on 12/16/17.
+//  Copyright © 2017 RedRooster Technologies Inc. All rights reserved.
+//
+
+import UIKit
+import LinkKit
+
+class ViewController: UIViewController, PLKPlaidLinkViewDelegate {
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    var linkConfigurationFromAppDelegate: PLKConfiguration! {
+        didSet {
+            print("Configurationfrom delegate was set")
+            self.loginButton.isEnabled = true
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.didReceiveNotification(_:)), name: NSNotification.Name(rawValue: "PLDPlaidLinkSetupFinished"), object: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @objc func didReceiveNotification(_ notification: NSNotification) {
+        if notification.name.rawValue == "PLDPlaidLinkSetupFinished" {
+            NotificationCenter.default.removeObserver(self, name: notification.name, object: nil)
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            self.linkConfigurationFromAppDelegate = appDelegate.LinkConfiguration!
+        }
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        let linkViewDelegate = self
+        let linkViewController = PLKPlaidLinkViewController(configuration: linkConfigurationFromAppDelegate, delegate: linkViewDelegate)
+        present(linkViewController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    //  MARK:- PLKPlaid delegates.
+    //  Metadata is important because it gives you information about what the heck is going on during the login flow. 
+    func linkViewController(_ linkViewController: PLKPlaidLinkViewController, didSucceedWithPublicToken publicToken: String, metadata: [String : Any]?) {
+        self.dismiss(animated: true) {
+            print(publicToken, metadata)
+        }
+    }
+    
+    func linkViewController(_ linkViewController: PLKPlaidLinkViewController, didExitWithError error: Error?, metadata: [String : Any]?) {
+        self.dismiss(animated: true) {
+            print(error, metadata)
+        }
+    }
+    
+    func linkViewController(_ linkViewController: PLKPlaidLinkViewController, didHandleEvent event: String, metadata: [String : Any]?) {
+        print(event, metadata)
+    }
+}
+
